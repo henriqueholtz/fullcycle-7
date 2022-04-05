346157bbb8d95768e1fd2774553a7bd47c1c8f13
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-05 05:41:44 -0300

    Add account.go and pixKey.go

---
+++ codepix/domain/model/pixKey.go
@@ -0,0 +1,45 @@
+package model
+
+type PixKey struct {
+	Base      `valid:"required"`
+	Kind      string   `json:"kind" valid:"notnull"`
+	Key       string   `json:"key" valid:"notnull"`
+	AccountID string   `json:"account_id" valid:"notnull"`
+	Account   *Account `valid:"-"`
+	Status    string   `json:"status" valid:"notnull"`
+}
+
+func (pixKey *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(pixKey)
+
+	if pixKey.Kind != "email" && pixKey.Kind != "cpf" {
+		return errors.New("Invalid type of key!")
+	}
+	if pixKey.Status != "active" && pixKey.Status != "inactive" {
+		return errors.New("Invalid status!")
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewPixKey(kind string, account *Account, key string) (*PixKey, error) {
+	pixKey := PixKey{
+		Kind:    kind,
+		Key:     key,
+		Account: account,
+		Status:  "active",
+	}
+
+	account.ID = uuid.NewV4().String()
+	account.CreatedAt = time.Now()
+
+	err := account.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &pixKey, nil
+}
