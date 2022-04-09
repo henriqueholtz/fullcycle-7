d4ef02ff1bbe2a2f9459794566c647a0786cd7cf
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-09 04:42:48 -0300

    Add table "bank_accounts" on "up" to first Migration (and rollback at "down"

--- bank-api/src/migrations/1649489091075-CreateBankAccountsTable.ts
+++ bank-api/src/migrations/1649489091075-CreateBankAccountsTable.ts
@@ -1,11 +1,41 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateBankAccountsTable1649489091075 implements MigrationInterface {
+export class CreateBankAccountsTable1649489091075
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'bank_accounts',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'account_number',
+            type: 'varchar(255)', //Default is 255
+          },
+          {
+            name: 'owner_name',
+            type: 'varchar(255)', //Default is 255
+          },
+          {
+            name: 'balance',
+            type: 'double precision',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('bank_accounts');
+  }
 }
