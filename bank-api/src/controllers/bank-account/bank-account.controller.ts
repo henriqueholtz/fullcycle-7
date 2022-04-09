3ce05e783d9ae464e5443fb41ff114c4b36deb3d
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-09 05:20:42 -0300

    Added global prefix: "api" and added endpoint "[GET] api/bank-accounts"

--- bank-api/src/controllers/bank-account/bank-account.controller.ts
+++ bank-api/src/controllers/bank-account/bank-account.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BankAccount } from 'src/models/bank-account.model';
+import { Repository } from 'typeorm';
 
-@Controller('bank-account')
-export class BankAccountController {}
+@Controller('bank-accounts')
+export class BankAccountController {
+  constructor(
+    @InjectRepository(BankAccount)
+    private bankAccountRepository: Repository<BankAccount>,
+  ) {}
+
+  @Get()
+  index() {
+    return this.bankAccountRepository.find();
+  }
+
+  //   @Get()
+  //   index() {}
+}
