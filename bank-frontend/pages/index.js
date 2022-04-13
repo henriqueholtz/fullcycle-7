582a14c7825f0b6e91e580dff27317ae66600d1f
henriqueholtz <henrique_holtz@hotmail.com>
2022-04-13 07:34:12 -0300

    [bank-frontend] Created next project (and remove some files "Welcome to Next.js") -> script "dev" at port 3001

---
+++ bank-frontend/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Bank-Frontend (FullCycle)</title>
+        <meta name="description" content="Bank-Frontend (FullCycle)" />
+      </Head>
+
+      <main>
+        <h1>Bank Frontend (FullCycle)</h1>
+      </main>
+    </div>
+  );
+}
